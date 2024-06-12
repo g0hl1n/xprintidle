g0hl1n/xprintidle

commit a1869c9fc166913962602a6314e98266ff756b41

Simple human-readable output

xprintidle.c
@@ -116,21 +116,60 @@ int get_x_idletime(uint64_t *idle) {
   return 0;
 }
 
+/* This function prints miliseconds in a human-readable format. */
+void print_human_time(uint64_t time) {
+  /* The C standard says that integer division round towards 0. */
+
+  int days = (int)(time / (24 * 60 * 60 * 1000));
+  time %= 24 * 60 * 60 * 1000;
+  if (days)
+    printf("%d days ", days);
+
+  int hours = (int)(time / (60 * 60 * 1000));
+  time %= 60 * 60 * 1000;
+  if (hours)
+    printf("%d hours ", hours);
+
+  int mins = (int)(time / (60 * 1000));
+  time %= 60 * 1000;
+  if (mins)
+    printf("%d minutes ", mins);
+
+  int secs = (int)(time / 1000);
+  time %= 1000;
+  if (secs)
+    printf("%d seconds ", secs);
+
+  int ms = (int)(time % 1000);
+  printf("%d milliseconds", ms);
+
+  printf("\n");
+}
+
 int main(int argc, char *argv[]) {
   uint64_t idle;
+  int human = 0;
 
   /* TODO change this to getopts as soon as we have more options */
   if (argc != 1) {
     if (!strcmp(argv[1], "-v") || !strcmp(argv[1], "--version")) {
       print_version();
       return EXIT_SUCCESS;
+    } else if (strcmp(argv[1], "--human-readable")) {
+      print_usage(argv[0]);
+      return EXIT_FAILURE;
     }
-    print_usage(argv[0]);
-    return EXIT_FAILURE;
+
+    human = 1;
   }
 
   if (get_x_idletime(&idle) < 0) {
     return EXIT_FAILURE;
+  }
+
+  if (human) {
+    print_human_time(idle);
+    return EXIT_SUCCESS;
   }
 
   printf("%lu\n", idle);

xprintidle.1
@@ -33,6 +33,9 @@ it to stdout (in milliseconds).
 .B \-h ", " \-\^\-help
 Output a usage message and exit.
 .TP
+.B \-\^\-human-readable
+Output the idle time in a human-readable format.
+.TP
 .B \-v ", " \-\^\-version
 Output the version number and exit.
 .

commit 7db95342e11452da3b89c6d4a645aa4064933db6

Fix human-readable output
Fix the error introduced when support for human readable output option was
introduced in a1869c9fc166 ("Simple human-readable output"), leading to the
option never working.

Additionally, document the option in the in-program help.

xprintidle.c
@@ -49,8 +49,9 @@ void print_usage(char *name) {
           "Query the X server for the user's idle time\n"
           "\n"
           "Options:\n"
-          "  -h, --help        Show this text\n"
-          "  -v, --version     Print the program version\n"
+          "  -h, --help              Show this text\n"
+          "  -H, --human-readable    Output the time in a human readable format\n"
+          "  -v, --version           Print the program version\n"
           "\n"
           "Report bugs at: https://github.com/g0hl1n/xprintidle/issues\n"
           "Written by Magnus Henoch and others; see\n"
@@ -158,12 +159,13 @@ int main(int argc, char *argv[]) {
     if (!strcmp(argv[1], "-v") || !strcmp(argv[1], "--version")) {
       print_version();
       return EXIT_SUCCESS;
-    } else if (strcmp(argv[1], "-H") && strcmp(argv[1], "--human-readable")) {
+    } else if (!strcmp(argv[1], "-H") || !strcmp(argv[1], "--human-readable")) {
+      human = 1;
+    } else if (!strcmp(argv[1], "-h") || !strcmp(argv[1], "--help")) {
       print_usage(argv[0]);
       return EXIT_FAILURE;
     }
 
-    human = 1;
   }
 
   if (get_x_idletime(&idle) < 0) {